c6847e6ccc3e3bb7bee7f9cdb6042655f2a37497
mco <muslumcanozata@gmail.com>
2024-03-07 11:02:02 +0300

    initial commit

---
+++ src/main/java/com/example/notelook/NotelookApplication.java
@@ -0,0 +1,13 @@
+package com.example.notelook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotelookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotelookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notelook/NotelookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notelook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotelookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
